cb60d1f459bb223e016e4653848137336da6d28d
NhuY05 <nguyenthinhuy.iuh@gmail.com>
2024-08-26 13:22:34 +0700

    Update stringb1c2.py

--- stringb1c2.py
+++ stringb1c2.py
@@ -6,5 +6,7 @@ Created on Thu Aug 22 18:53:00 2024
 """
 
 print("bai2Câu 2: ")
-chuoi= "Đại học Quốc gia, Khu phố 6, P.Linh Trung, Q.Thủ Đức, Tp.HCM"
-print(chuoi.replace(", ","\n" ).replace("P.","").replace("Q.","").replace("Tp.",""))
+s = "Đại học Quốc gia, Khu phố 6, P.Linh Trung, Q.Thủ Đức, Tp.HCM"
+s1 = s.replace("P. ","" ).replace("P.","").replace("Q.","").replace("Tp.","").split(", ")
+for i in s1:
+    print(i)
