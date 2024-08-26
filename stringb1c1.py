f5bb15e05ca3f45fca5b1f32c8387579dc901b4e
NhuY05 <nguyenthinhuy.iuh@gmail.com>
2024-08-26 13:24:53 +0700

    Update stringb1c1.py

--- stringb1c1.py
+++ stringb1c1.py
@@ -5,8 +5,11 @@ Created on Thu Aug 22 18:37:49 2024
 @author: Vivobook
 """
 print("B1 Câu 1: ")
-chuoi= "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
-print(chuoi.replace(', ','\n'))
+chuoi1 = "Đại học Quốc gia, Khu phố 6, P. Linh Trung, Q. Thủ Đức, Tp. HCM"
+s1 = chuoi1.split(", ")
+for i in s1:
+    print(i)
+
 
 
 
